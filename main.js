b7abb22f86018b2eea8d1b99c71cdaec11725c15
Buddhalimbu <53037805+buddhalimbu@users.noreply.github.com>
2023-04-22 22:13:06 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 //JavaScript
 
+const gensimple = document.querySelector(".gen-simple");
+const genunique = document.querySelector(".gen-unique");
+
+const cvbtns = document.querySelector(".cv-btns");
+
+const inputimg = document.querySelector("#hide-on-simple");
+
+const resumeS = document.querySelector(".resumeS");
+const iwraps = document.querySelector(".input-wraps");
+//logging
+const loading = document.querySelector(".loading");
+const resume = document.querySelector(".resume");
+const errorpage = document.querySelector(".errorpage");
+
+
+
 function display(){
   let fileUpload = document.querySelector(".input-img").value;
   //alert(fileUpload);
@@ -10,10 +26,9 @@ document.querySelector(".red").innerHTML=fileUpload;
 
 }
 
-
 //inputs first 
-function generate(){
-  
+function generateU(){
+
 const inputname = document.querySelector('.input-name').value;
 
 const inputage= document.querySelector('.input-age').value;
@@ -36,7 +51,8 @@ const inputcollage = document.querySelector('.input-collage').value;
 
 const inputcomputer = document.querySelector('.input-computer').value;
 
-const inputwork= document.querySelector('.input-work').value;
+const fw1= document.querySelector('.input-work1').value;
+const fw2 = document.querySelector(".input-work2").value;
 
 //items after clicks
 const img = document.querySelector(".img img");
@@ -65,12 +81,8 @@ const clg= document.querySelector(".clg");
 
 const cp = document.querySelector(".cp");
 
-const work = document.querySelector(".work");
-
-//logging
-const loading = document.querySelector(".loading");
-const resume = document.querySelector(".resume");
-const iwraps = document.querySelector(".input-wraps");
+const fwork = document.querySelector(".fw1");
+const swork = document.querySelector(".fw2");
 
 const btngen = document.querySelector(".btn-gen1");
 
@@ -78,12 +90,18 @@ const btngen2 = document.querySelector(".btn-gen2");
 
 var f = document.querySelector(".input-img");
 
-const errorpage = document.querySelector(".errorpage");
+const uidno = document.querySelector(".idno").value;
+const ums = document.querySelector(".ms").value;
+
+const uidnoholder = document.querySelector(".uidnoholder");
+const umsholder = document.querySelector(".umsholder");
+
+
 //initiate the itmes
 
 if(inputname==="" || inputage==="" || inputadd==="" || inputbg==="" || inputdate==="" || inputemail==="" || inputphone==="" || inputnationality==="" || inputfname==="" || f.files.length===0){
   errorpage.classList.add("show");
-  errorpage.innerHTML = "Alert !! Check your details.<br/> Reloading page.......! ";
+  errorpage.innerHTML = "<div> Error, Loading ... </div><span class='spin'></span>";
   setTimeout(()=>{
     errorpage.classList.remove("show")
   },4000);
@@ -110,12 +128,15 @@ else {
   bgroup.innerHTML=inputbg;
   phone.innerHTML=inputphone;
   email.innerHTML=inputemail;
+  uidnoholder.innerHTML=uidno;
+umsholder.innerHTML=ums;
   nationality.innerHTML=inputnationality;
   clg.innerHTML=inputcollage;
   cp.innerHTML=inputcomputer;
-  work.innerHTML=inputwork;
+  fwork.innerHTML=fw1;
+  swork.innerHTML=fw2;
   loading.classList.add("show");
-  iwraps.classList.add("hide");
+  iwraps.classList.remove("visible");
   
   
   window.scrollTo(0, loading.scrollHeight);
@@ -125,8 +146,116 @@ else {
   setTimeout(()=>{
     loading.classList.remove("show");
     resume.classList.add("show");
-    btngen.classList.add("show");
-    btngen2.classList.add("show")
+    genunique.classList.add("visible");
+    
+  },4000);
+  
+  
+}
+  
+  
+}
+
+
+//Simple GEN
+function generateS(){
+const inputname = document.querySelector('.input-name').value;
+
+const inputage= document.querySelector('.input-age').value;
+
+const inputdate = document.querySelector('.input-date').value;
+
+const inputfname = document.querySelector('.input-fname').value;
+
+const inputadd = document.querySelector('.input-add').value;
+
+const inputbg= document.querySelector('.input-bg').value;
+
+const inputphone = document.querySelector('.input-phone').value;
+
+const inputemail = document.querySelector('.input-email').value;
+
+const inputnationality = document.querySelector('.input-nationality').value;
+
+const inputcollage = document.querySelector('.input-collage').value;
+
+const inputcomputer = document.querySelector('.input-computer').value;
+
+const work1= document.querySelector('.input-work1').value;
+const work2 = document.querySelector(".input-work2").value;
+
+  
+const names = document.querySelector(".names");
+
+
+const ages = document.querySelector(".ages");
+
+const dofs = document.querySelector(".dobs");
+
+const adds = document.querySelector(".adds");
+
+const fnames = document.querySelector(".fnames");
+
+const bgroups = document.querySelector(".bgroups");
+
+const emails = document.querySelector(".emails");
+const phones = document.querySelector(".phones");
+const nationalitys = document.querySelector(".nationalitys");
+
+
+const clgs = document.querySelector(".clgs");
+
+const cps = document.querySelector(".cps");
+const firstwork = document.querySelector(".firstwork");
+const secondwork = document.querySelector(".secondwork");
+
+const idno = document.querySelector(".idno").value;
+const ms = document.querySelector(".ms").value;
+
+const idnoholder = document.querySelector(".idnoholder");
+const msholder = document.querySelector(".msholder");
+
+
+if(inputname==="" || inputage==="" || inputadd==="" || inputbg==="" || inputdate==="" || inputemail==="" || inputphone==="" || inputnationality==="" || inputfname===""){
+  errorpage.classList.add("show");
+  errorpage.innerHTML = "<div> Error, Loading ... </div><span class='spin'></span>";
+  setTimeout(()=>{
+    errorpage.classList.remove("show")
+  },4000);
+}
+
+else {
+  
+  
+  //merge values of input 
+  names.innerHTML= inputname;
+  ages.innerHTML=inputage;
+  adds.innerHTML=inputadd;
+  dofs.innerHTML=inputdate;
+  fnames.innerHTML=inputfname;
+  bgroups.innerHTML=inputbg;
+  phones.innerHTML=inputphone;
+  emails.innerHTML=inputemail;
+  nationalitys.innerHTML=inputnationality;
+  idnoholder.innerHTML=idno;
+msholder.innerHTML=ms;
+  clgs.innerHTML=inputcollage;
+  cps.innerHTML=inputcomputer;
+  firstwork.innerHTML=work1;
+  secondwork.innerHTML=work2;
+  loading.classList.add("show");
+  iwraps.classList.remove("visible");
+  
+  
+  window.scrollTo(0, loading.scrollHeight);
+  
+  
+  //setimeout
+  setTimeout(()=>{
+    loading.classList.remove("show");
+    resumeS.classList.add("visible");
+    gensimple.classList.add("visible");
+  
   },4000)
   
   
@@ -138,14 +267,31 @@ else {
 
 
 
-function check(){
+
+
+
+//Check
+function check(x){
   var f = document.querySelector(".input-img");
+  var imgholder = document.querySelector(".imgholder");
+  var imgholderImg = imgholder.querySelector("img");
+  
   if(f.files.length == 0){
-    document.querySelector(".alert").innerHTML = " Failed : Not found Img ";
+    document.querySelector(".alert").innerHTML = " Failed : Img Not Found. ";
   }
   else {
-    document.querySelector(".alert").innerHTML = " Success : " + f.value;
-    document.querySelector(".alert").classList.add("color")
+
+    document.querySelector(".alert").innerHTML = " Success : " + f.value + "Img Found.";
+    document.querySelector(".alert").classList.add("color");
+    imgholder.classList.add("flex");
+    let reader = new FileReader();
+  let input = document.querySelector(".input-img");
+    reader.onload = () => {
+      let img = document.querySelector('.imgholder img');
+      img.src = reader.result;
+    };
+    reader.readAsDataURL(input.files[0]);
+  
   }
 }
 
@@ -156,7 +302,7 @@ const orihtml = d.innerHTML;
 d.innerHTML = "Downloading..";
 setTimeout(()=>{
   d.innerHTML=orihtml
-},4000);
+},5000);
     const captureElement = document.querySelector(".resume");
 
     html2canvas(captureElement)
@@ -183,3 +329,76 @@ setTimeout(()=>{
         });
 
 }
+
+//Download S
+
+function downloadS() {
+const d = document.querySelector(".download");
+const orihtml = d.innerHTML;
+d.innerHTML = "Downloading..";
+setTimeout(()=>{
+  d.innerHTML=orihtml
+},4000);
+    const captureElement = document.querySelector(".resumeS");
+
+    html2canvas(captureElement)
+        .then((canvas) => {
+            canvas.style.display = "none";
+
+            document.body.appendChild(canvas);
+
+            return canvas;
+        })
+
+        .then((canvas) => {
+            const image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+
+            const a = document.createElement("a");
+
+            a.setAttribute("download", "my-image.png");
+
+            a.setAttribute("href", image);
+
+            a.click();
+
+            canvas.remove();
+        });
+
+}
+
+
+
+function genS(){
+  cvbtns.classList.add("hide");
+  inputimg.classList.add("hide");
+  iwraps.classList.add("visible");
+  document.querySelector(".genS").classList.add("visible");
+  errorpage.classList.add("show");
+  errorpage.innerHTML = "<span class='spin'></span>";
+  setTimeout(()=>{
+    errorpage.classList.remove("show")
+  },4000);
+}
+
+function genU(){
+  cvbtns.classList.add("hide");
+  iwraps.classList.add("visible");
+  document.querySelector(".genU").classList.add("visible");
+  errorpage.classList.add("show");
+  errorpage.innerHTML = "<span class='spin'></span>";
+  setTimeout(()=>{
+    errorpage.classList.remove("show")
+  },4000);
+}
+
+
+
+function Closeme(){
+  var x = document.querySelector(".imgholder").classList.remove("flex")
+}
+
+function openov(){
+  document.querySelector('.ov').style.display='flex'
+}
+
+
